6c45b77aec6cc17032dcadbb5fbc774f435402a5
Gerardo Rodriguez <55673748+g3rrarrd@users.noreply.github.com>
2023-12-02 16:36:46 -0600

    Add files via upload

---
+++ softbytes_backed_ss/SoftbytesBackendApplication.java
@@ -0,0 +1,13 @@
+package hn.softbytes.softbytes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftbytesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftbytesBackendApplication.class, args);
+	}
+
+}
